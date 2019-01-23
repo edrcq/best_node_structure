6b03e4a47fab4408e53f6c1b02929979e4ed71e4
Edouard Rucquoy <erucquoy@gmail.com>
2019-01-23 17:40:07 +0100

    add mongo

--- app/index.js
+++ app/index.js
@@ -1,1 +1,17 @@
-const app = require('./server')
+const { startMongo } = require('./mongo')
+
+let app = null; 
+
+async function initApp () {
+    await createDatasFolders()
+    startMongo(function(err) {
+        if (err) {
+            console.error(err);
+            process.exit(1)
+            return ;
+        }
+        app = require('./server')
+    })
+}
+
+initApp ()
